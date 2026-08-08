45800d0eab43f3e01bdbfaf885e6fe0c4c621700
khan1203 <rk.rifat1203@gmail.com>
2026-08-08 21:19:56 +0600

    merge conflicts cleared

--- app/models/resume.py
+++ app/models/resume.py
@@ -13,16 +13,7 @@ class Resume(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     candidate_id: Mapped[int] = mapped_column(ForeignKey("candidates.id", ondelete="CASCADE"), index=True)
     filename: Mapped[str] = mapped_column(String(255), nullable=False)
-<<<<<<< HEAD
-    filepath: Mapped[str] = mapped_column(String(512), nullable=False)
-=======
-<<<<<<< HEAD
-    filepath: Mapped[str] = mapped_column(String(512), nullable=False)
-=======
-#   filepath: Mapped[str] = mapped_column(String(512), nullable=False)
     s3_key: Mapped[str] = mapped_column(String(512), nullable=False)
->>>>>>> 7e52a1e (resume stored into aws_s3_buckets)
->>>>>>> 379ca77 (resumes stored in aws s3 bucket)
     size_bytes: Mapped[int] = mapped_column(Integer, default=0)
     is_primary: Mapped[bool] = mapped_column(Boolean, default=False)
     uploaded_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
